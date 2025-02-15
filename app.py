a2bdce52068cad1e0832ac2970121adcafe3c37f
rhmvvCYB3R <10ragimov10@gmail.com>
2025-02-15 22:15:28 +0100

    НАЧАЛОООО

--- app.py
+++ app.py
@@ -1,2 +1,19 @@
-import aiogram
+from aiogram import Bot, Dispatcher
 import asyncio
+import config
+from aiogram.types import Message
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from aiogram.filters import CommandStart
+
+bot = Bot(token=config.BOT_TOKEN)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def cmd_start(message: Message):
+    await message.answer("SALAM!")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
