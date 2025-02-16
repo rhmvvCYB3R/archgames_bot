6dc5b56d02b258894a0c459569f55c7d32223b18
rhmvvCYB3R <10ragimov10@gmail.com>
2025-02-16 16:27:05 +0100

    HANDLERS пока нахуй не нужен

--- app/handlers.py
+++ app/handlers.py
@@ -1,18 +1,18 @@
-from aiogram import F, Router
-from aiogram.filters import CommandStart,Command
-from aiogram.types import Message
-import app.keyboards
+# from aiogram import F, Router
+# from aiogram.filters import CommandStart,Command
+# from aiogram.types import Message
+# import app.keyboards
 
 
 
-router = Router()
+# router = Router()
 
-@router.message(CommandStart())
-async def cmd_start(message: Message):
-    await message.reply(f'Привет {message.from_user.first_name}. \nДобро пожаловать в Мир азартных приключений!', reply_markup= app.keyboards.main)
+# @router.message(CommandStart())
+# async def cmd_start(message: Message):
+#     await message.reply(f'Привет {message.from_user.first_name}. \nДобро пожаловать в Мир азартных приключений!', reply_markup= app.keyboards.main)
 
 
-@router.message(Command('help'))
-async def get_help(message: Message):
-    await message.answer('OK!')
+# @router.message(Command('help'))
+# async def get_help(message: Message):
+#     await message.answer('OK!')
 
